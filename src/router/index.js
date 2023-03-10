ea29afaea917ca17a8a35b7915d74c38fda7ca01
Keith Price <keithprice.dev@gmail.com>
2023-03-10 15:35:38 +0700

    added document titles for the existing routes - Home, Blogs

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Blogs from "../views/Blogs.vue"
+import Blogs from "../views/Blogs.vue";
 
 Vue.use(VueRouter);
 
@@ -10,11 +10,17 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home",
+    },
   },
   {
     path: "/blogs",
     name: "Blogs",
     component: Blogs,
+    meta: {
+      title: "Blogs",
+    },
   },
 ];
 
@@ -22,6 +28,11 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | WritingBlog`;
+  next();
 });
 
 export default router;
