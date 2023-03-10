916f31cc5f31d547120d2760589a4c7c5a335858
Keith Price <keithprice.dev@gmail.com>
2023-03-10 17:24:40 +0700

    added correct routinhg to footer for Home and Blogs

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -29,8 +29,8 @@
                 </div>
                 <div class="col-2">
                     <ul>
-                        <router-link class="link" :to="{ name: 'home' }">Home</router-link>
-                        <router-link class="link" :to="{ name: 'blogs' }">Blogs</router-link>
+                        <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
+                        <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
                         <router-link v-if="user" class="link" :to="{ name: 'newpost' }">Create Post</router-link>
                         <router-link v-if="!user" class="link" :to="{ name: 'login' }">Login/Register</router-link>
                     </ul>
