c7b9443549fd1dbb2b289bd6d17176834d2b07c5
Keith Price <keithprice.dev@gmail.com>
2023-03-11 16:20:51 +0700

    added views for login, register and forgot-password

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="app-wrapper">
-    <div class="app">
-      <Navigation />
-      <router-view />
-      <Footer />
+    <div class="app-wrapper">
+        <div class="app">
+            <Navigation v-if="!navigationDisabled" />
+            <router-view />
+            <Footer v-if="!navigationDisabled" />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -13,15 +13,30 @@ import Navigation from "./components/Navigation.vue";
 import Footer from "./components/Footer.vue";
 
 export default {
-  name: "app",
-  components: { Navigation, Footer },
-  data() {
-    return {};
-  },
-  created() { },
-  mounted() { },
-  methods: {},
-  watch: {},
+    name: "app",
+    components: { Navigation, Footer },
+    data() {
+        return { navigationDisabled: null };
+    },
+    created() {
+        this.checkRoute()
+    },
+    mounted() { },
+    methods: {
+        checkRoute() {
+            if (this.$route.name === 'Login' || this.$route.name === 'Register' || this.$route.name === 'ForgotPassword') {
+                this.navigationDisabled = true
+            }
+            else {
+                this.navigationDisabled = false
+            }
+        }
+    },
+    watch: {
+        $route() {
+            this.checkRoute()
+        }
+    },
 };
 </script>
 
@@ -29,127 +44,128 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
 
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Quicksand", sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Quicksand", sans-serif;
 }
 
 .app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
 }
 
 .container {
-  max-width: 1440px;
-  margin: 0 auto;
+    max-width: 1440px;
+    margin: 0 auto;
 }
 
 .link {
-  cursor: pointer;
-  text-decoration: none;
-  text-transform: uppercase;
-  color: black;
+    cursor: pointer;
+    text-decoration: none;
+    text-transform: uppercase;
+    color: black;
 }
 
 .link-light {
-  color: #fff;
+    color: #fff;
 }
 
 .arrow {
-  margin-left: 8px;
-  width: 12px;
+    margin-left: 8px;
+    width: 12px;
 
-  path {
-    fill: #000;
-  }
+    path {
+        fill: #000;
+    }
 }
 
 .arrow-light {
-  path {
-    fill: #fff;
-  }
+    path {
+        fill: #fff;
+    }
 }
 
 .button,
 .router-button {
-  transition: .5s ease all;
-  cursor: pointer;
-  margin-top: 24px;
-  padding: 12px 24px;
-  background-color: #303030;
-  color: #fff;
-  border-radius: 20px;
-  border: none;
-  text-transform: uppercase;
-  text-decoration: none;
+    transition: .5s ease all;
+    cursor: pointer;
+    margin-top: 24px;
+    padding: 12px 24px;
+    background-color: #303030;
+    color: #fff;
+    border-radius: 20px;
+    border: none;
+    text-transform: uppercase;
+    text-decoration: none;
 
-  &:focus {
-    outline: none;
-  }
+    &:focus {
+        outline: none;
+    }
 
-  &:hover {
-    background-color: rgba(48, 48, 48, 0.7);
-  }
+    &:hover {
+        background-color: rgba(48, 48, 48, 0.7);
+    }
 }
 
 .button-ghost {
-  color: #000;
-  padding: 0;
-  border-radius: 0;
-  margin-top: 50px;
-  font-size: 15px;
-  font-weight: 500;
-  background-color: transparent;
+    color: #000;
+    padding: 0;
+    border-radius: 0;
+    margin-top: 50px;
+    font-size: 15px;
+    font-weight: 500;
+    background-color: transparent;
 
-  @media (min-width: 700px) {
-    margin-top: 0;
-    margin-left: auto;
-  }
+    @media (min-width: 700px) {
+        margin-top: 0;
+        margin-left: auto;
+    }
 
-  i {
-    margin-left: 8px;
-  }
+    i {
+        margin-left: 8px;
+    }
 }
 
 .button-light {
-  background-color: transparent;
-  border: 2px solid #fff;
-  color: #fff;
+    background-color: transparent;
+    border: 2px solid #fff;
+    color: #fff;
 }
 
 .button-inactive {
-  pointer-events: none !important;
-  cursor: none !important;
-  background-color: rgba(128, 128, 128, 0.5) !important;
+    pointer-events: none !important;
+    cursor: none !important;
+    background-color: rgba(128, 128, 128, 0.5) !important;
 }
 
 .blog-card-wrap {
-  position: relative;
-  padding: 80px 16px;
-  background-color: #f1f1f1;
-
-  @media(min-width: 500px) {
-    padding: 100px 16px;
-  }
-
-  .blog-cards {
-    display: grid;
-    gap: 32px;
-    grid-template-columns: 1fr;
+    position: relative;
+    padding: 80px 16px;
+    background-color: #f1f1f1;
 
     @media(min-width: 500px) {
-      grid-template-columns: repeat(2, 1fr);
+        padding: 100px 16px;
     }
 
-    @media(min-width: 900px) {
-      grid-template-columns: repeat(3, 1fr);
+    .blog-cards {
+        display: grid;
+        gap: 32px;
+        grid-template-columns: 1fr;
+
+        @media(min-width: 500px) {
+            grid-template-columns: repeat(2, 1fr);
+        }
+
+        @media(min-width: 900px) {
+            grid-template-columns: repeat(3, 1fr);
+        }
+
+        @media(min-width: 1200px) {
+            grid-template-columns: repeat(4, 1fr);
+        }
     }
 
-    @media(min-width: 1200px) {
-      grid-template-columns: repeat(4, 1fr);
-    }
-  }
-
-}</style>
+}
+</style>
